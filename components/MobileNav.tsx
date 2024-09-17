ef64f5183b2fbd468a2fa368b29514e0d7b0dc7a
RavshanSean <ravshansean1995@gmail.com>
2024-09-17 05:33:11 -0400

    navbarFooter

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -2,6 +2,7 @@
 
 import {
     Sheet,
+    SheetClose,
     SheetContent,
     SheetDescription,
     SheetHeader,
@@ -36,37 +37,45 @@ const pathname = usePathname();
                width={34}
                height={34}
                alt='Horizon logo'
-              
             />
             <h1 className='text-26 font-ibm-plex-serif font-bold text-black-1'>PinkOceanBlue</h1>
           </Link>
-          {sidebarLinks.map((item) => {
-            const isActive = pathname === item.route || pathname.startsWith(`${item.route}/`)
+          <div className="mobilenav-sheet">
+            <SheetClose asChild>
+                <nav className="flex h-full flex-col gap-6 pt-16 text-white">
+                {sidebarLinks.map((item) => {
+                 const isActive = pathname === item.route || pathname.startsWith(`${item.route}/`)
 
-            return (
-                <Link href={item.route} key={item.label}
-                className={cn('sidebar-link', {'bg-bank-gradient' : isActive })}>
+              return (
+                <SheetClose asChild key={item.route}>
+                 <Link href={item.route} key={item.label}
+                 className={cn('mobilenav-sheet_close w-full', {'bg-bank-gradient' : isActive })}>
                     
-                  <div className='relative size-6'>
                      <Image 
                       src={item.imgURL}
                       alt={item.label}
-                      fill
+                      width={20}
+                      height={20}
                       className={cn({
                         'brightness-[3] invert-0':isActive
                       })}
                      />
-                  </div>
-                  <p className={cn("sidebar-label", { "!text-white": isActive })}>
+                  <p className={cn("text-16 font-semibold text-black-2", { "text-white": isActive })}>
                     {item.label}
                   </p>
                 </Link>
+                </SheetClose>
             )
           })}
 
+                  USER
+                </nav>
+               </SheetClose>
+
+               FOOTER
+             </div>
            </SheetContent>
         </Sheet>
-
     </section>
   )
 }
